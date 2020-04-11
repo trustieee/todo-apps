a9e66adfe0d96009d5d4ac449f5f5ff817a6eed9
mariocatch <mcacciatore@edatadimensions.com>
2020-04-10 22:29:34 -0400

    Add local storage of todos to blazor wasm

--- todo-blazor-wasm/todo-blazor-wasm/Models/TodoItemModel.cs
+++ todo-blazor-wasm/todo-blazor-wasm/Models/TodoItemModel.cs
@@ -5,6 +5,7 @@ namespace todo_blazor_wasm.Models
     public class TodoItemModel
     {
         public Guid Id { get; } = Guid.NewGuid();
+
         public string Title { get; set; }
     }
 }
--- todo-blazor-wasm/todo-blazor-wasm/wwwroot/js/TodoList.JSInterop.js
+++ todo-blazor-wasm/todo-blazor-wasm/wwwroot/js/TodoList.JSInterop.js
@@ -1,5 +1,14 @@
 ﻿window.todoListJsInterop = {
     focus: function (element) {
         element.focus();
+    },
+
+    get: function (defaultValue, key) {
+        const stickyValue = window.localStorage.getItem(key);
+        return stickyValue ?? defaultValue;
+    },
+
+    set: function (key, value) {
+        window.localStorage.setItem(key, JSON.stringify(value));
     }
 }
